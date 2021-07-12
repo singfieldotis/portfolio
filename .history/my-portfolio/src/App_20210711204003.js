14c106b1750a771a3d375f8b8bb3e003c9548194
Otis Singfield <singfieldotis@gmail.com>
2021-07-11 20:42:32 -0400

    finally initial commit and buidling site

--- .history/my-portfolio/src/App_20210711204003.js
+++ .history/my-portfolio/src/App_20210711204003.js
@@ -9,12 +9,12 @@ import Footer from "./components/Footer";
 export default function App() {
   return (
     <main>
-      <Header />
+      <Navbar />
       <About />
       <Projects />
-      <Resume />
+      <Skills />
+      <Testimonials />
       <Contact />
-      <Footer />
     </main>
   );
 }
