685a74f67d84678290943f2a4856234921639e1f
Otis Singfield <singfieldotis@gmail.com>
2021-07-11 22:42:58 -0400

    page operational

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import About from "./components/About";
+import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
@@ -8,13 +8,19 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <main>
-      <Header />
-      <About />
-      <Projects />
-      <Resume />
-      <Contact />
-      <Footer />
-    </main>
+    <div>
+      <header>
+        <Header />
+      </header>
+      <main className="text-gray-400 bg-gray-900 body-font">
+        <Home />
+        <Projects />
+        <Resume />
+        <Contact />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   );
 }
