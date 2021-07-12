fab3cb1daa15d8ed931077d19933c2cfe10b261a
Otis Singfield <singfieldotis@gmail.com>
2021-07-11 22:46:25 -0400

    fixing

---
+++ .history/my-portfolio/src/App_20210711205528.js
@@ -0,0 +1,20 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Header />
+      <About />
+      <Projects />
+      <Resume />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
